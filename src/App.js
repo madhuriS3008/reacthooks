da6ea0c2b78a620673bf5a7871367317083a8486
Madhuri Sabbineni <madhusabbineni@gmail.com>
2024-02-05 22:06:20 -0600

    using useEffect hook to change webpage title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import GiftCard from "./components/GiftCard";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    document.title = toggle
+      ? "Welcome to little lemon"
+      : "Using useEffect hook";
+  }, [toggle]);
+
+  const toggleHandler = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className="App">
+      <h1>Little Lemon</h1>
+      <button onClick={toggleHandler}>Toggle message</button>
+      {toggle && <p>Welcome to little lemon.</p>}
       <GiftCard />
     </div>
   );
